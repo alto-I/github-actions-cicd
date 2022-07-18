4b0fda365a0a97d525e6dd57cd8ea53bc898ea4f
alto-I <blmaldiy@gmail.com>
2022-07-18 10:38:52 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+modele github.com/alto-I/github-actions-cicd
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+import "fmt"
+func Greeting() string {
+	return "hello"
+}
+func main() {
+	fmt.Println(Greeting())
+}
